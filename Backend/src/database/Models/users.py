ce499d31ac1a757e9e0d1d5acf58cdc11f439859
Maicol <maicolcubidespe@gmail.com>
2024-02-07 20:22:30 -0500

    1.0.4 Maicol

--- Backend/src/database/Models/users.py
+++ Backend/src/database/Models/users.py
@@ -3,4 +3,5 @@ from mongoengine import Document, StringField, FileField
 
 class Usuario(Document):
     apodo = StringField(required=True)
+    password = StringField(required=True) 
     foto = FileField()
--- Frontend/FrontendReact/src/main.jsx
+++ Frontend/FrontendReact/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './Components/css/login.css'
-// import App from './App.jsx'
+//import App from './App.jsx'
 import Login from './Components/Login' 
 import './index.css'
 
