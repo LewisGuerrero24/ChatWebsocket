cf1f26b3ea2a5ee0cf8018f328253c0a96393242
Lewis Guerrero <lewisguerrero20@gmail.com>
2024-06-19 22:06:38 -0500

    --version 19/06/2024

---
+++ Backend/src/database/Repository/UserRepository.py
@@ -0,0 +1,9 @@
+class UserRepository():
+    def __init__(self, TemporalUsuario):
+        self.TemporalUsuario = TemporalUsuario
+
+    def existUser(self, data):
+        usuario_existente = self.TemporalUsuario.objects(nombre=data['name']).first()
+        if usuario_existente:
+            return usuario_existente
+        return usuario_existente
--- Backend/src/database/conexion.py
+++ Backend/src/database/conexion.py
@@ -1,13 +1,11 @@
 from Libraries import *
-from decouple import config
+from dotenv import load_dotenv
+import os
 
 def con():
     try:
-        connect(host=config('MONGODB_HOST'))
+        connect(host=os.getenv('MONGODB_HOST'))
         print('Conectado a la base de datos de MongoDB')
     except Exception as e:
         print(f'Error al conectar a la base de datos: {e}')
         return None
-
-
-   
