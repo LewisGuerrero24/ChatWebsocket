d54dd5db7989ee0ac94fdafd6b2ec90fa35080d3
Lewis Guerrero <lewisguerrero20@gmail.com>
2024-07-22 20:45:44 -0500

    cambio dashboardEstudiante

--- Frontend/FrontendReact/src/Components/DashboardEstudiante.jsx
+++ Frontend/FrontendReact/src/Components/DashboardEstudiante.jsx
@@ -1,41 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import tokenUtils from "../Hooks/utils";
-import { useNavigate } from 'react-router-dom';
-import logoutUsers from "../Helpers/logoutUsers";
+import { useNavigate, useParams } from 'react-router-dom';
+import axios from 'axios'; 
+import ListContact from './UserProfile/ListContact';
+import './css/chat.css';
+import Chat from './Chat';
+import useRoom from '../Hooks/useRoom';
+import useConnect from '../Hooks/useConnect';
+import ChatUsers from './ChatUsers';
 
-const DashboardEstudiante = () => {
+const DashboardUser = () => {
     const isLoggedIn = tokenUtils.checkIfIsLoggedIn();
     const user = tokenUtils.getLoggedInUserId();
     const navigate = useNavigate();
     const [lastActive, setLastActive] = useState(new Date());
+    const { connected, socket } = useConnect();
+    const { name, Room } = useParams();
+    const [urlRoom, setUrlRoom] = useState(Room);;
 
-    // const handleLogout = async () => {
-    //     try {
-    //         // Realiza una solicitud POST a la ruta /logout para cerrar sesión
-    //         await axios.post('http://localhost:5000/logout');
+    const handleLogout = async () => {
+        try {
+            // Realiza una solicitud POST a la ruta /logout para cerrar sesión
+            await axios.post('http://localhost:5000/logout');
             
-    //         // Elimina el token del almacenamiento local
-    //         tokenUtils.removeToken();
-    //         setLastActive(new Date());
-    //         // Redirige al usuario a la página de inicio
-    //         navigate('/');
+            // Elimina el token del almacenamiento local
+            tokenUtils.removeToken();
+            setLastActive(new Date());
+            // Redirige al usuario a la página de inicio
+            navigate('/');
 
-    //     // Reemplaza la entrada actual del historial con la página de inicio
-    //     window.history.pushState({}, '', '/');
+        // Reemplaza la entrada actual del historial con la página de inicio
+        window.history.pushState({}, '', '/');
         
-    //     // Recarga la página para asegurarse de que se aplique el cambio en el historial
-    //     window.location.reload();
-    //     } catch (error) {
-    //         console.error('Error during logout:', error);
-    //     }
-    // }
+        // Recarga la página para asegurarse de que se aplique el cambio en el historial
+        window.location.reload();
+        } catch (error) {
+            console.error('Error during logout:', error);
+        }
+    }
 
 
 
     useEffect(() => {
         const interval = setInterval(() => {
             if (isLoggedIn && (new Date() - lastActive) >= 2 * 60 * 1000) {
-                logoutUsers(setLastActive, navigate);
+                handleLogout();
             }
         }, 1000);
 
@@ -63,17 +72,18 @@ const DashboardEstudiante = () => {
       }
   }, [isLoggedIn]);
 
-  const handleLogout = () => {
-    logoutUsers(setLastActive, navigate);
-  };
-
     return (
         <>
-            <h1>Holaaaa Estudiante</h1>
             <nav>
                 {isLoggedIn && (
                     <>
-                        <div>Bienvenido <b>{user}</b></div>
+                        <div className="flex h-screen antialiased text-gray-800">
+                            <div className="flex flex-row h-full w-full overflow-x-hidden">
+                                <ListContact connected={connected} name={name}/>
+                                <h1>{name}</h1>
+                                <ChatUsers key={urlRoom} nameSendUser={"pedro"} name={name} connected={connected} socket={socket} nameRoom={urlRoom}/>
+                            </div>
+                        </div>
                         <button onClick={handleLogout}>Cerrar sesión</button> 
                     </>
                 )}
@@ -82,4 +92,4 @@ const DashboardEstudiante = () => {
     );
 }
 
-export default DashboardEstudiante;
+export default DashboardUser;
